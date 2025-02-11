bd23bfdae6f6181df306f054a847364a8cdf6b4b
darkbater <nc@clear-pc.ru>
2025-02-11 21:54:00 +0300

    modified:   test-postgres.php

--- test-postgres.php
+++ test-postgres.php
@@ -1,11 +1,15 @@
 <?php
 include 'vendor/autoload.php';
+// echo(__DIR__);
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+// print("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}". $_ENV['PG_USER']. $_ENV['PG_PASSWORD']);
+
+$pdo = new PDO("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}", $_ENV['PG_USER'], $_ENV['PG_PASSWORD']);
+
 
 // var_dump($_ENV);
 
-echo(__DIR__);
 // $PG_HOST    = getenv('PG_HOST');
 // $PG_DBNAME  = getenv('PG_DBNAME');
 // $PG_USER    = getenv('PG_USER');
