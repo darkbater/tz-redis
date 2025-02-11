80bdee71844e4687a6ed46069d6b5f1d8230a8a3
darkbater <nc@clear-pc.ru>
2025-02-11 22:03:47 +0300

    modified:   test-postgres.php
    test_table_create

--- test-postgres.php
+++ test-postgres.php
@@ -12,6 +12,24 @@ $pdo = new PDO("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}", $_EN
 // Создать 4 таблицы на postgresql:
 // продукты, категории, статистика, заказы, и заполнить стандартными столбцами на ваш выбор.
 
+if($pdo->exec("CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            description TEXT,
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );")) echo "products complete\n";
+
+// $pdo->exec("CREATE TABLE IF NOT EXISTS menu (
+//             id SERIAL PRIMARY KEY,
+//             caption VARCHAR(255) NOT NULL,
+//             parrent VARCHAR(255) NOT NULL,
+//             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//             );")
+
+
+
+
 // В таблице Заказы добавить колонку с временем покупки.
 
 // Вам необходимо написать триггер на таблицу с заказами:
