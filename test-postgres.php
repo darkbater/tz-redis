efc2807c43412958f8e42b54230b07a80016b32a
darkbater <nc@clear-pc.ru>
2025-02-11 22:10:13 +0300

    modified:   test-postgres.php
    + pg table products init

--- test-postgres.php
+++ test-postgres.php
@@ -7,25 +7,33 @@ $dotenv->load();
 // print("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}". $_ENV['PG_USER']. $_ENV['PG_PASSWORD']);
 
 $pdo = new PDO("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}", $_ENV['PG_USER'], $_ENV['PG_PASSWORD']);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 // Создать 4 таблицы на postgresql:
 // продукты, категории, статистика, заказы, и заполнить стандартными столбцами на ваш выбор.
 
-if($pdo->exec("CREATE TABLE IF NOT EXISTS products (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            description TEXT,
-            price DECIMAL(10, 2) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            );")) echo "products complete\n";
 
-// $pdo->exec("CREATE TABLE IF NOT EXISTS menu (
-//             id SERIAL PRIMARY KEY,
-//             caption VARCHAR(255) NOT NULL,
-//             parrent VARCHAR(255) NOT NULL,
-//             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//             );")
+try{
+    // Таблица продуктов
+    $pdo->exec("DROP TABLE IF EXISTS products");
+    if($pdo->exec("CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                description TEXT,
+                price DECIMAL(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );")) echo "products complete\n";
+
+
+
+
+
+
+
+} catch (PDOException $e) {
+    die("Error creating any table: " . $e->getMessage());
+}    
 
 
 
