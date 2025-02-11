ef3c7998df8aba198afd3538cfc69ca1c7ca235d
darkbater <nc@clear-pc.ru>
2025-02-12 01:36:55 +0300

    modified:   test-postgres.php pre test

--- test-postgres.php
+++ test-postgres.php
@@ -10,13 +10,12 @@ $pdo = new PDO("pgsql:host={$_ENV['PG_HOST']};dbname={$_ENV['PG_DBNAME']}", $_EN
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-
 // Создать 4 таблицы на postgresql:
 // продукты, категории, статистика, заказы, и заполнить стандартными столбцами на ваш выбор.
 
-/// [ ] создаём таблицы
+/// [v] создаём таблицы
 /// [v] добавляем тригер
-/// [ ] добавляем в статистику инфу по заказам
+/// [v] добавляем в статистику инфу по заказам
 /// [ ] создаём таблицы
 /// [ ] создаём таблицы
 /// [ ] создаём таблицы
@@ -30,15 +29,15 @@ $pdo->exec("CREATE TABLE IF NOT EXISTS test (
 try{
     
     // Таблица категорий
-    $pdo->exec("DROP TABLE IF EXISTS categories CASCADE;");
+    // $pdo->exec("DROP TABLE IF EXISTS categories CASCADE;");
     $pdo->exec("CREATE TABLE IF NOT EXISTS categories (
                     id SERIAL PRIMARY KEY,
                     name VARCHAR(255) NOT NULL,
                     description TEXT);");
 
-
+    echo('cat');
     // Таблица продуктов
-    $pdo->exec("DROP TABLE IF EXISTS products");
+    // $pdo->exec("DROP TABLE IF EXISTS products CASCADE");
     if($pdo->exec("CREATE TABLE IF NOT EXISTS products (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
@@ -48,16 +47,27 @@ try{
                 );")) echo "products complete\n";
 
     // Таблица заказов
-    // TODO таблица заказов
-    $pdo->exec("DROP TABLE IF EXISTS orders");
+    $pdo->exec("DROP TABLE IF EXISTS orders CASCADE");
     if($pdo->exec("CREATE TABLE IF NOT EXISTS orders (
                     id SERIAL PRIMARY KEY,
                     -- user_id INT NOT NULL,
+                    product_id INT REFERENCES products(id),
+                    count INT NOT NULL,
                     total_price DECIMAL(10, 2) NOT NULL,
-                    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     status VARCHAR(50) DEFAULT 'pending',
                     purchase_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 );")) echo "orders complete\n";
+
+
+    // Таблица статистики
+    $pdo->exec("DROP TABLE IF EXISTS statistics CASCADE");
+    if($pdo->exec("CREATE TABLE IF NOT EXISTS statistics (
+                id SERIAL PRIMARY KEY,
+                category_id INT REFERENCES categories(id),
+                count INT NOT NULL,
+                date DATE DEFAULT CURRENT_DATE,
+                UNIQUE (category_id, date)  
+                );")) echo "statistics complete\n";
 
 
 // Триггер на заказы
@@ -67,13 +77,59 @@ $pdo->exec("DROP FUNCTION IF EXISTS upset_statistic");
 if($pdo->exec("CREATE FUNCTION upset_statistics()
                         RETURNS trigger AS
                         $$
+                        DECLARE
+                            _category INT;
+                            _date DATE;
+
                         BEGIN
-                            INSERT INTO test(test) values('test');
+                            --- id категории из продуктов
+                            SELECT category_id INTO _category
+                            FROM products
+                            WHERE id = NEW.product_id;
+
+                            _date=NEW.purchase_time;
+
+
+                            INSERT INTO statistics (category_id, date, count)
+                            VALUES (_category, _date, NEW.count)
+                            ON CONFLICT (category_id, date)
+                            DO UPDATE SET 
+                                count = statistics.count + NEW.count;
+
+
+                            -- INSERT INTO test(test) values('test');
+                            -- INSERT INTO statistics(category_id, count, date)
+                            --     SELECT 
+                            --         category_id,
+                            --         count,
+                            --         CURRENT_DATE
+                            --     FROM orders
+                            --     WHERE id = NEW.id
+
+
                             RETURN NEW;
                         END;
                         $$
                         LANGUAGE plpgsql;
                         ")) echo "trigger appended\n";
+
+
+// INSERT INTO tablename (id, username, password, level, email, update_count) 
+
+// -- if id doesn't exist, do insert
+// VALUES (1, 'John', 'qwerty', 5, 'john@mail.com', 0) 
+
+// -- how to check for duplicates (more versatile: could use any unique index here)
+// ON CONFLICT (id) 
+// DO UPDATE 
+// SET 
+//   -- update duplicate clause
+//   username=EXCLUDED.username, -- references proposed insertion row
+//   password=EXCLUDED.password,
+//   level=EXCLUDED.level,
+//   email=EXCLUDED.email,
+//   update_count=tablename.update_count+1 -- reference existing row
+
 
 $pdo->exec("DROP TRIGGER IF EXISTS order_statistic_upset_trigger ON orders;");
 if($pdo->exec("CREATE TRIGGER order_statistic_upset_trigger
@@ -87,11 +143,11 @@ if($pdo->exec("CREATE TRIGGER order_statistic_upset_trigger
 
 
 
-
 } catch (PDOException $e) {
     die("Error creating any table: " . $e->getMessage());
 }    
 
+die("complete");
 
 
 
